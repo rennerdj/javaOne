834e434bc691cc48427a3e44067c533351d4596e
mydjr <mydjr@DJRPC1>
2018-09-09 20:19:28 -0500

    Created a New Method BedCabError and changed icon type to WARNING_MESSAGE

--- src/vehicles/CreateATruck.java
+++ src/vehicles/CreateATruck.java
@@ -5,6 +5,7 @@
  */
 package vehicles;
 
+import java.awt.HeadlessException;
 import javax.swing.JOptionPane;
 
 /**
@@ -34,15 +35,19 @@ public class CreateATruck {
         final Object selectedBedLength = JOptionPane.showInputDialog(null, "Choose a Bed Length", "Bed Selection", JOptionPane.QUESTION_MESSAGE, null, bedLength, MEDIUM__BED);
 
         if (selectedCab.equals(DOUBLE_CAB) && selectedBedLength.equals(LONG__BED)) {
-                JOptionPane.showMessageDialog(null, "Sorry but you cannot choose both a Double Cab and Long Bed");
-            } else if (selectedCab.equals(REGULAR) && selectedBedLength.equals(SHORT__BED)) {
-                JOptionPane.showMessageDialog(null, "Sorry, but you cannot choose both a Regular Cab and A Short Bed.");
-            } else if (selectedCab.equals(SUPERCAB) && selectedBedLength.equals(SHORT__BED)) {
-                JOptionPane.showMessageDialog(null, "Sorry, but you cannot choose both a Super Cab and A Short Bed.");
-            } else {
-            JOptionPane.showMessageDialog(null, "You Chose a SuperCab");
+            BedAndCabError(selectedCab, selectedBedLength);
+        } else if (selectedCab.equals(REGULAR) && selectedBedLength.equals(SHORT__BED)) {
+            BedAndCabError(selectedCab, selectedBedLength);
+        } else if (selectedCab.equals(SUPERCAB) && selectedBedLength.equals(SHORT__BED)) {
+            BedAndCabError(selectedCab, selectedBedLength);
+        } else {
+            JOptionPane.showMessageDialog(null, "Your selections are valid. Cab: " + selectedCab + " Bed: " + selectedBedLength);
         }
-}
+    }
+
+    private static void BedAndCabError(Object selectedCab, final Object selectedBedLength) throws HeadlessException {
+        JOptionPane.showMessageDialog(null, "Sorry but you cannot choose both a " + selectedCab + " and " + selectedBedLength, "invalid combination", JOptionPane.WARNING_MESSAGE);
+    }
     private static final String LONG__BED = "Long Bed";
     private static final String MEDIUM__BED = "Medium Bed";
     private static final String SHORT__BED = "Short Bed";
